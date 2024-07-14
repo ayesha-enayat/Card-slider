1de1f1a16036f59ecd9b53f6793934ab93cedc29
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-07-14 09:54:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    //Responsive break points
+    breakpoints: {
+      0: {
+        slidesPerView:1
+      },
+      768: {
+        slidesPerView:2
+      },
+      1024: {
+        slidesPerView:3
+      },
+
+    }
+  });
